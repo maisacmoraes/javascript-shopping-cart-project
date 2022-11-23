e79fa654568f4e3168fab6dfd2556dde6916af51
Maisa Moraes <maisa.moraes@outlook.com>
2022-11-23 10:41:03 -0300

    implementa 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,24 +1,26 @@
-const cartAdress = document.querySelector('.cart__address');
-
 export const getAddress = async (cep) => {
-  const promise1 = fetch(`https://cep.awesomeapi.com.br/json/${cep}`);
-  const promise2 = fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`);
-  const promises = [promise1, promise2];
-  const response = await Promise.any(promises);
-  const data = await response.json();
-  return data;
+  try {
+    const promise1 = fetch(`https://cep.awesomeapi.com.br/json/${cep}`);
+    const promise2 = fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`);
+    const promises = [promise1, promise2];
+    const response = await Promise.any(promises);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error.message;
+  }
 };
 
 export const searchCep = async (cep) => {
+  const cartAdress = document.querySelector('.cart__address');
   const address = await getAddress(cep);
-
-  if (address.message) {
-    cartAdress.innerHTML = 'CEP não encontrado';
-  } else {
+  if (address.state) {
     const apiRoad = address.address || address.street;
     const apiDistrict = address.district || address.neighborhood;
-    const apirCity = address.city;
+    const apiCity = address.city;
     const apiState = address.state;
-    cartAdress.innerHTML = `${apiRoad} - ${apiDistrict} - ${apirCity} - ${apiState}`;
+    cartAdress.innerHTML = `${apiRoad} - ${apiDistrict} - ${apiCity} - ${apiState}`;
+  } else {
+    cartAdress.innerHTML = 'CEP não encontrado';
   }
 };
